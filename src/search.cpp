d4d7bfbd071f9b4f72d3f86b67b61866f4ccfd01
Fabian Fichter <ianfab@users.noreply.github.com>
2021-01-23 22:34:59 +0100

    Tweak NMP for variants

--- src/search.cpp
+++ src/search.cpp
@@ -933,7 +933,7 @@ namespace {
         && (ss-1)->statScore < 22977
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 30 * depth - 28 * improving + 84 * ss->ttPv + 168 + 200 * (!pos.double_step_enabled() && pos.piece_to_char()[PAWN] != ' ')
+        &&  ss->staticEval >= beta - 30 * depth - 28 * improving + 84 * ss->ttPv + 168 + 200 * (!pos.double_step_enabled() && pos.piece_to_char()[PAWN] != ' ') + 200 * pos.blast_on_capture() + 200 * pos.check_counting()
         && !excludedMove
         &&  pos.non_pawn_material(us)
         &&  pos.count<ALL_PIECES>(~us) != pos.count<PAWN>(~us)
